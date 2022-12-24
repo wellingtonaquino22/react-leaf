904e6263b8d404b51727800671df63696a7f5450
Wellington <welaquino202122@gmail.com>
2022-12-24 11:32:42 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1 className='title'>TESTE REACT LEAFLET</h1>
+    <div className='leafletContainer'>
+    <MapContainer  
+    style={{height: "100%", width: "100%"}}
+    center={[-8.11389, -35.2915]} 
+    zoom={15} 
+    scrollWheelZoom={false}>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+  <Marker position={[-8.11623995128, -35.2719688109]}>
+    <Popup>
+      Localização Marcada.
+    </Popup>
+  </Marker>
+</MapContainer>
+</div>
+</>
+  )
 }
 
 export default App;
